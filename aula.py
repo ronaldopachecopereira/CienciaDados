22b53231490173285bff4a8973d00eb35a947cfe
Ronaldo Pereira <108314419+ronaldopachecopereira@users.noreply.github.com>
2022-08-25 18:51:59 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,13 @@
+
+func = str(input("Digite o nome do funcionário: "))
+idade =  int(input("Digite a idade do funcionário: "))
+tarefa = str(input("Digite a função do funcionário: "))
+enderec= str(input("Digite o endereço do funcionário: "))
+salario = (input("Digite o salário do funcionário: "))
+print(
+      f"Nome: {func} " 
+      f"Função: {tarefa} " 
+      f"Idade: {idade} "
+      f"Enderec: {enderec} "
+      f"Salário: {salario} "
+     )
